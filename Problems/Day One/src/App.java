15d91d6256c84618e80a28d1e65d5a31f3238cfb
suehtemorp <javier.solanosaltachin@ucr.ac.cr>
2023-12-15 23:56:20 -0600

    Implemented Day One problem

---
+++ Problems/Day One/src/App.java
@@ -0,0 +1,89 @@
+/// Scan charset string inputs for integers
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class App {
+    public static void main(String[] args) throws Exception 
+    {
+        /// Indicate user to input the document
+        System.out.println("Enter the calibration document below.");
+        System.out.println("EOF will be taken as the end of input.");
+        
+        /// Keep track of the total calibration sum
+        Integer totalSum = 0;
+
+        /// Update the sum for each line until it runs out
+        /// To do so, scan each line until no more digits exist
+        Scanner integerScanner = new Scanner(System.in);
+        Pattern digitPattern = Pattern.compile("\\d");
+        
+        for (Integer lineIndex = 1; integerScanner.hasNextLine(); 
+            lineIndex += 1)
+        {
+            /// Buffer each digit match
+            String lineMatch;
+
+            /// Capture first digit
+            Integer leftmostDigit = null;
+
+            lineMatch = integerScanner.findInLine(digitPattern);
+            if (lineMatch != null)
+            {
+                leftmostDigit 
+                    = Integer.parseInt(lineMatch);
+            }
+
+            /// If there is no first digit, the line is ill-informed
+            /// and we may not proceed
+            else
+            {
+                throw new Exception
+                (
+                    String.format
+                    (
+                        "Document is ill-informed! At line %d", 
+                        lineIndex
+                    )
+                );
+            }
+
+
+            /// Capture last digit past the first digit
+            Integer rightmostDigit = null;
+
+            for (lineMatch = integerScanner.findInLine(digitPattern); 
+                lineMatch != null; 
+                lineMatch = integerScanner.findInLine(digitPattern))
+            {
+                rightmostDigit
+                    = Integer.parseInt(lineMatch);
+            }
+
+            /// If no last digit is found past the first digit, then
+            /// the last digit is by definition the first digit
+            if (rightmostDigit == null)
+            { rightmostDigit = leftmostDigit; }
+
+            /// Me may now construct the current line calibration value
+            /// and inject it right away into the total sum
+            totalSum += leftmostDigit * 10 + rightmostDigit;
+
+            /// After processing the current line, we may carry onto the
+            /// next one
+            integerScanner.nextLine();
+        }
+
+        /// After all lines have been scanned, we may now close the scanner
+        /// and report the net sum of all the lines' calibration values 
+        integerScanner.close();
+
+        System.out.println
+        (
+            String.format
+            (
+                "The net calibration value is %d", 
+                totalSum
+            )
+        );
+    }
+}
